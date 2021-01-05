381c34bdcca78302d20b0b7d236c8f73639afa8c
藤井大樹 <fujiidaiki@fujiidaikinoMacBook-Pro.local>
2021-01-05 22:45:58 +0900

    chatHeader

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import Sidebar from './Sidebar';
+import Sidebar from './Sidebar/Sidebar';
+import Chat from './Chat/Chat';
 
 function App() {
   return (
     <div className="app">
       <Sidebar />
+      <Chat />
     </div>
   );
 }
